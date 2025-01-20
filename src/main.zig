5ba2ad8ca963067e74d33117a16a244bc0abe86a
timosdev99 <timothyisah4@gmail.com>
2025-01-20 16:16:15 +0100

    fixed main for now

--- src/main.zig
+++ src/main.zig
@@ -2,15 +2,14 @@ const std = @import("std");
 const FileExplorer = @import("file_explorer.zig").FileExplorer;
 
 pub fn main() !void {
-    // Initialize general purpose allocator
+    // Initialize general-purpose allocator
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
 
     // Get current directory as starting point
-
-    var buf: [std.fs.max_path_bytes]u8 = undefined;
-    const current_dir = try std.os.getcwd(&buf);
+    const current_dir = try std.fs.cwd().realpathAlloc(allocator, ".");
+    defer allocator.free(current_dir);
 
     // Initialize file explorer
     var explorer = try FileExplorer.init(allocator, current_dir);
@@ -21,8 +20,8 @@ pub fn main() !void {
     try explorer.listDirectory();
 
     // Try to get info about a specific file
-    std.debug.print("\nGetting file info for 'file_explorer.zig':\n", .{});
-    explorer.getFileInfo("file_explorer.zig") catch |err| {
+    std.debug.print("\nGetting file info for 'file':\n", .{});
+    explorer.getFileInfo(current_dir) catch |err| {
         std.debug.print("Error getting file info: {}\n", .{err});
     };
 }
